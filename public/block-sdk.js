7af8014ca31dbfced995b2ac1d5dc39c64f28d65
Sushant Reddy <63240393+sushant-reddy@users.noreply.github.com>
2025-05-16 16:29:03 +0530

    Update block-sdk.js

--- public/block-sdk.js
+++ public/block-sdk.js
@@ -1,137 +1,249 @@
-/**
- * Salesforce Marketing Cloud Block SDK
- * File: block-sdk.js
- * 
- * This is a modified version of the Salesforce Marketing Cloud Block SDK
- * that works in both development and production environments.
- * 
- * Original source: https://github.com/salesforce-marketingcloud/blocksdk
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root  or https://opensource.org/licenses/BSD-3-Clause
  */
 
-(function(window, document) {
-  const SDK = function(config, whitelistOverride, blockEditorBaseURL) {
-    // Set defaults
-    this.config = config || {};
-    this.blockEditorBaseURL = blockEditorBaseURL || 'https://blocksdk.marketingcloudapps.com';
-    this._whitelistOverride = whitelistOverride || false;
-    this._parentOrigin = 'https://mc.exacttarget.com';
-    this._messageId = 1;
-    this._messages = {};
-    this._readyToPost = false;
-    this._pendingMessages = [];
+var SDK = function (config, whitelistOverride, sslOverride) {
+	// config has been added as the primary parameter
+	// If it is provided ensure that the other paramaters are correctly assigned
+	// for backwards compatibility
+	if (Array.isArray(config)) {
+		whitelistOverride = config;
+		sslOverride = whitelistOverride;
+		config = undefined;
+	}
 
-    // Initialize
-    this._init();
-  };
+	if (config && config.onEditClose) {
+		this.handlers = {
+			onEditClose: config.onEditClose
+		};
+		config.onEditClose = true;
+	}
 
-  SDK.prototype = {
-    // Initialize SDK
-    _init: function() {
-      const self = this;
+	this._whitelistOverride = whitelistOverride;
+	this._sslOverride = sslOverride;
+	this._messageId = 1;
+	this._messages = {
+		0: function () {}
+	};
+	this._readyToPost = false;
+	this._pendingMessages = [];
+	this._receiveMessage = this._receiveMessage.bind(this);
 
-      // Handle messages from parent window
-      window.addEventListener('message', function(event) {
-        if (event.origin !== self._parentOrigin) {
-          return;
-        }
+	window.addEventListener('message', this._receiveMessage, false);
 
-        // Handle specific message types
-        const data = event.data;
-        if (data.type === 'ready') {
-          self._readyToPost = true;
-          self._processQueue();
-        } else if (data.type === 'response') {
-          const message = self._messages[data.messageId];
-          if (message && message.callback) {
-            message.callback(data.payload);
-          }
-          delete self._messages[data.messageId];
-        }
-      });
+	window.parent.postMessage({
+		method: 'handShake',
+		origin: window.location.origin,
+		payload: config
+	}, '*');
+};
 
-      // Notify parent window that the SDK is ready
-      if (window.parent) {
-        this._post('ready');
-      }
-    },
+SDK.prototype.execute = function execute (method, options) {
+	options = options || {};
 
-    // Post a message to parent window
-    _post: function(type, payload, callback) {
-      const message = {
-        id: this._messageId++,
-        type: type,
-        payload: payload || {}
-      };
+	var self = this;
+	var payload = options.data;
+	var callback = options.success;
 
-      this._messages[message.id] = {
-        callback: callback
-      };
+	if (!this._readyToPost) {
+		this._pendingMessages.push({
+			method: method,
+			payload: payload,
+			callback: callback
+		});
+	} else {
+		this._post({
+			method: method,
+			payload: payload
+		}, callback);
+	}
+};
 
-      if (this._readyToPost) {
-        window.parent.postMessage(message, this._parentOrigin);
-      } else {
-        this._pendingMessages.push(message);
-      }
-    },
+SDK.prototype.getCentralData = function (cb) {
+	this.execute('getCentralData', {
+		success: cb
+	});
+};
 
-    // Process queue of pending messages
-    _processQueue: function() {
-      const self = this;
-      this._pendingMessages.forEach(function(message) {
-        window.parent.postMessage(message, self._parentOrigin);
-      });
-      this._pendingMessages = [];
-    },
+SDK.prototype.getContent = function (cb) {
+	this.execute('getContent', {
+		success: cb
+	});
+};
 
-    /**
-     * Public SDK Methods
-     */
+SDK.prototype.getData = function (cb) {
+	this.execute('getData', {
+		success: cb
+	});
+};
 
-    // Get content from Block SDK
-    getContent: function(callback) {
-      this._post('getContent', null, callback);
-    },
+SDK.prototype.getUserData = function (cb) {
+	this.execute('getUserData', {
+		success: cb
+	});
+};
 
-    // Set content in Block SDK
-    setContent: function(content, callback) {
-      this._post('setContent', { content: content }, callback);
-    },
+SDK.prototype.getView = function (cb) {
+	this.execute('getView', {
+		success: cb
+	});
+};
 
-    // Get data from Block SDK
-    getData: function(callback) {
-      this._post('getData', null, callback);
-    },
+SDK.prototype.setBlockEditorWidth = function (value, cb) {
+	this.execute('setBlockEditorWidth', {
+		data: value,
+		success: cb
+	});
+};
 
-    // Set data in Block SDK
-    setData: function(data, callback) {
-      this._post('setData', { data: data }, callback);
-    },
+SDK.prototype.setCentralData = function (dataObj, cb) {
+	this.execute('setCentralData', {
+		data: dataObj,
+		success: cb
+	});
+};
 
-    // Get height of Block SDK
-    getHeight: function(callback) {
-      this._post('getHeight', null, callback);
-    },
+SDK.prototype.setContent = function (content, cb) {
+	this.execute('setContent', {
+		data: content,
+		success: cb});
+};
 
-    // Set height of Block SDK
-    setHeight: function(height, callback) {
-      this._post('setHeight', { height: height }, callback);
-    },
+SDK.prototype.setData = function (dataObj, cb) {
+	this.execute('setData', {
+		data: dataObj,
+		success: cb
+	});
+};
 
-    // Get super content from Block SDK
-    getSuperContent: function(callback) {
-      this._post('getSuperContent', null, callback);
-    },
+SDK.prototype.setSuperContent = function (content, cb) {
+	this.execute('setSuperContent', {
+		data: content,
+		success: cb
+	});
+};
 
-    // Set super content in Block SDK
-    setSuperContent: function(content, callback) {
-      this._post('setSuperContent', { content: content }, callback);
-    }
-  };
+SDK.prototype.triggerAuth = function (appID) {
+	this.getUserData(function (userData) {
+		var stack = userData.stack;
+		if (stack.indexOf('qa') === 0) {
+			stack = stack.substring(3,5) + '.' + stack.substring(0,3);
+		}
+		var iframe = document.createElement('IFRAME');
+		iframe.src = 'https://mc.' + stack + '.exacttarget.com/cloud/tools/SSO.aspx?appId=' + appID + '&restToken=1&hub=1';
+		iframe.style.width= '1px';
+		iframe.style.height = '1px';
+		iframe.style.position = 'absolute';
+		iframe.style.top = '0';
+		iframe.style.left = '0';
+		iframe.style.visibility = 'hidden';
+		iframe.className = 'authframe';
+		document.body.appendChild(iframe);
+	});
+};
 
-  // Create BlockSDK namespace if it doesn't exist
-  if (!window.sfdc) {
-    window.sfdc = {};
-  }
+SDK.prototype.triggerAuth2 = function (authInfo) {
+	var iframe = document.createElement('IFRAME');
+	var scope = '';
+	var state = '';
+	if(Array.isArray(authInfo.scope)) {
+		scope = '&scope=' + authInfo.scope.join('%20');
+	}
+	if(authInfo.state) {
+		state = '&state=' + authInfo.state;
+	}
+	iframe.src = authInfo.authURL + (authInfo.authURL.endsWith('/') ? '':'/') + 'v2/authorize?response_type=code&client_id=' + authInfo.clientId + '&redirect_uri=' + encodeURIComponent(authInfo.redirectURL) + scope + state;
+	iframe.style.width= '1px';
+	iframe.style.height = '1px';
+	iframe.style.position = 'absolute';
+	iframe.style.top = '0';
+	iframe.style.left = '0';
+	iframe.style.visibility = 'hidden';
+	iframe.className = 'authframe';
+	document.body.appendChild(iframe);
+};
 
-  window.sfdc.BlockSDK = SDK;
-})(window, document);
+/* Internal Methods */
+
+SDK.prototype._executePendingMessages = function _executePendingMessages () {
+	var self = this;
+
+	this._pendingMessages.forEach(function (thisMessage) {
+		self.execute(thisMessage.method, {
+			data: thisMessage.payload,
+			success: thisMessage.callback
+		});
+	});
+
+	this._pendingMessages = [];
+};
+
+SDK.prototype._post = function _post (payload, callback) {
+	this._messages[this._messageId] = callback;
+	payload.id = this._messageId;
+	this._messageId += 1;
+	// the actual postMessage always uses the validated origin
+	window.parent.postMessage(payload, this._parentOrigin);
+};
+
+SDK.prototype._receiveMessage = function _receiveMessage (message) {
+	message = message || {};
+	var data = message.data || {};
+
+	if (data.method === 'handShake') {
+		if (this._validateOrigin(data.origin)) {
+			this._parentOrigin = data.origin;
+			this._readyToPost = true;
+			this._executePendingMessages();
+			return;
+		}
+	} else if (data.method === 'closeBlock') {
+		if (this._validateOrigin(data.origin)) {
+			// here execute the method before closing the  block editing
+			//onEditClose();
+			if (this.handlers && this.handlers.onEditClose) {
+				this.handlers.onEditClose();
+			}
+			this.execute('blockReadyToClose');
+			return;
+		}
+	}
+
+	// if the message is not from the validated origin it gets ignored
+	if (!this._parentOrigin || this._parentOrigin !== message.origin) {
+		return;
+	}
+	// when the message has been received, we execute its callback
+	(this._messages[data.id || 0] || function () {})(data.payload);
+	delete this._messages[data.id];
+};
+
+// the custom block should verify it is being called from the marketing cloud
+SDK.prototype._validateOrigin = function _validateOrigin (origin) {
+	// Make sure to escape periods since these strings are used in a regular expression
+	var allowedDomains = this._whitelistOverride || ['exacttarget\\.com', 'marketingcloudapps\\.com', 'blocktester\\.herokuapp\\.com'];
+
+	for (var i = 0; i < allowedDomains.length; i++) {
+		// Makes the s optional in https
+		var optionalSsl = this._sslOverride ? '?' : '';
+		var mcSubdomain = allowedDomains[i] === 'exacttarget\\.com' ? 'mc\\.' : '';
+		var whitelistRegex = new RegExp('^https' + optionalSsl + '://' + mcSubdomain + '([a-zA-Z0-9-]+\\.)*' + allowedDomains[i] + '(:[0-9]+)?$', 'i');
+
+		if (whitelistRegex.test(origin)) {
+			return true;
+		}
+	}
+
+	return false;
+};
+
+if (typeof(window) === 'object') {
+	window.sfdc = window.sfdc || {};
+	window.sfdc.BlockSDK = SDK;
+}
+if (typeof(module) === 'object') {
+	module.exports = SDK;
+}
